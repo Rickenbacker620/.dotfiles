9f9d254cdca071329626c4707fb39c28d0c768be
Rickenbacker620 <1057558227@qq.com>
2023-08-18 15:23:27 +0800

    cleanup config

--- fish/.config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-fish_config theme choose "Dracula"
-
-function fish_command_not_found
-    echo -e "Oops! Command \033[31m$argv[1]\033[0m not found"
-end
-
-if type -q pyenv
-    pyenv init - | source
-end
