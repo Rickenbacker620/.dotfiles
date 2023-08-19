3b0240f78c439c1200df7d5cd31786e46d10f60c
Rickenbacker620 <1057558227@qq.com>
2023-08-19 16:42:49 +0800

    add picom

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,12 @@
 fish_config theme choose "Dracula"
 
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
+
 function fish_command_not_found
     echo -e "Oops! Command \033[31m$argv[1]\033[0m not found"
 end
