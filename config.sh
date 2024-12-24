692ea7a51d0c86b4768943b87e72e7f707a49c76
Rickenbacker620 <fu78sion@gmail.com>
2024-12-24 06:54:18 +0000

    fixed config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ] || ! groups | grep -qE 'sudo|wheel'; then
+if [ "$EUID" -ne 0 ] && ! groups | grep -qE 'sudo|wheel'; then
   echo "This script must be run as root or by a user who can execute sudo."
   exit 1
 fi
