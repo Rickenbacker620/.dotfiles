878c94deb06c9484cb74d819293a8dea0b2ec40e
Rickenbacker620 <fu78sion@gmail.com>
2024-12-24 12:02:29 -0500

    added yazi keymap

--- config.sh
+++ config.sh
@@ -62,6 +62,7 @@ function install_base() {
 
     if [ $LINUX_DIST == debian ]; then
         curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        . "$HOME/.cargo/env"
         rustup update
         cargo install --locked yazi-fm yazi-cli
     fi
