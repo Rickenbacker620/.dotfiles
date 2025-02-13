a52d49370455d75441779f5aec80c4219cd7433c
Rickenbacker620 <fu78sion@gmail.com>
2025-02-12 19:27:19 -0500

    removed mongodb

--- config.sh
+++ config.sh
@@ -144,20 +144,6 @@ function install_dev() {
         # UV
         info "Installing uv"
         curl -LsSf https://astral.sh/uv/install.sh | sh
-
-        # MongoDB
-        info "Installing MongoDB"
-        curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
-        gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
-        --dearmor
-
-        echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" | tee /etc/apt/sources.list.d/mongodb-org-8.0.list
-        sudo apt-get update
-        sudo apt-get install -y mongodb-org
-    fi
-
-    if [ $LINUX_DIST == arch ]; then
-        $AUR_INSTALL mongodb-bin
     fi
 }
 
